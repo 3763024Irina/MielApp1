e1d392bc91ab8d696f6a53dad08a9af30869ae85
IriSakovich <sakovich@gmx.es>
2025-07-09 14:57:41 +0200

    9/07/2025

---
+++ MielApp1/QuotaScreen.swift
@@ -0,0 +1,21 @@
+//
+//     QuotaScreen.swift
+//  MielApp1
+//
+//  Created by Irina on 7/7/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct QuotaScreen: View {
+    var body: some View {
+        VStack {
+            Text("Квоты")
+                .font(.largeTitle.bold())
+            Spacer()
+            // Здесь данные по квотам
+        }
+        .navigationTitle("Квоты")
+    }
+}
